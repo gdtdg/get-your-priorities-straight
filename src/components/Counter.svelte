c9397c74ec5be138319abde27becf2b94b63ea9f
vdtdg <valerian@dtdg.fr>
2025-03-04 23:25:12 +0100

    Playing a bit with writable to understand the difference with $state

---
+++ src/components/Counter.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { writable } from 'svelte/store';
+
+  // Creating a writable store
+  export const counter = writable(0);
+
+  function increment() {
+    counter.update(n => n + 1);
+  }
+</script>
+
+<!-- Using the store -->
+<button on:click={increment}>Increment</button>
+<p>Count: {$counter}</p>
