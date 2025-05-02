936732d33e9e566a48951ae13724e159b02b4f55
Valérian de Thézan de Gaussan <valerian@dtdg.fr>
2025-05-02 17:04:39 +0200

    Added Umami monitoring

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script>
   import "../app.css";
+  import { PUBLIC_UMAMI_SCRIPT_URL, PUBLIC_UMAMI_WEBSITE_ID } from '$env/static/public';
   let { children } = $props();
 </script>
+
+<svelte:head>
+  <script defer src={PUBLIC_UMAMI_SCRIPT_URL} data-website-id={PUBLIC_UMAMI_WEBSITE_ID}></script>
+</svelte:head>
 
 {@render children()}
